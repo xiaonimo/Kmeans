cdcabc2b302ca22927ba0bb2bd790c5c5830598b
ZhiTong <958456112@qq.com>
2018-04-04 21:15:53 +0800

    bugs exist

--- kdtree.hpp
+++ kdtree.hpp
@@ -41,16 +41,22 @@ public:
     inline KDTree(points_t _dataset):dataset(_dataset), data_dim(_dataset[0].size()), data_cnt(_dataset.size()){}
     void build_KDTree(const unsigned int max_leaf_size=1);
     points_t search(const point_t&, const unsigned int topk=1);
+    indexes_t search_index(const point_t&, const unsigned int topk=1);
     void print();
+    unsigned tree_min_depth();
+    unsigned tree_max_depth();
 
 private:
     index_t _get_max_variance_dim(const indexes_t&);
     std::pair<index_t, data_t> _get_split_val_index(const index_t dim, const indexes_t& indexes);
-    TreeNode<data_t>* _build_KDTree(const indexes_t&, const unsigned int max_leaf_size);
+    //TreeNode<data_t>* _build_KDTree(const indexes_t&, const unsigned int max_leaf_size);
+    void _build_KDTree(TreeNode<data_t>* &, const indexes_t&, const unsigned int max_leaf_size);
     void _search(const point_t&, const unsigned int, TreeNode<data_t>*, std::vector<std::pair<index_t, data_t>> &res);
     void _print(const TreeNode<data_t>*);
     void _print(const point_t p);
     data_t Metric(point_t, point_t);
+    unsigned _tree_max_depth(TreeNode<data_t>*);
+    unsigned _tree_min_depth(TreeNode<data_t>*);
 
 private:
     TreeNode<data_t>* head;
@@ -63,9 +69,11 @@ template<typename data_t>
 void KDTree<data_t>::build_KDTree(const unsigned int max_leaf_size) {
     indexes_t indexes;
     for (index_t i=0; i<dataset.size(); ++i) indexes.push_back(i);
-    head = _build_KDTree(indexes, max_leaf_size);
+    _build_KDTree(head, indexes, max_leaf_size);
+    std::cout << "KDTree build finished!" <<std::endl;
 }
 
+/*
 template<typename data_t>
 TreeNode<data_t>* KDTree<data_t>::_build_KDTree(const indexes_t& indexes, const unsigned int max_leaf_size) {
     //处理空数据集
@@ -98,6 +106,40 @@ TreeNode<data_t>* KDTree<data_t>::_build_KDTree(const indexes_t& indexes, const 
 
     //递归完成后，返回头结点
     return _head;
+}*/
+
+template<typename data_t>
+void KDTree<data_t>::_build_KDTree(TreeNode<data_t>* &_head, const indexes_t& indexes, const unsigned int max_leaf_size) {
+    //处理空数据集
+    if (indexes.empty()) {
+        return;
+    }
+    //处理叶节点
+    if (indexes.size() <= max_leaf_size) {
+        _head = new TreeNode<data_t>(data_t(-1), data_t(-1));
+        for (auto __index:indexes) _head->index_set.push_back(__index);
+        return;
+    }
+    //创建头结点
+    index_t _dim = _get_max_variance_dim(indexes);
+    std::pair<index_t, data_t> _val_index = _get_split_val_index(_dim, indexes);
+    _head = new TreeNode<data_t>(_dim, _val_index.second);
+    _head->index_set.push_back(_val_index.first);
+
+    //递归创建左子树、右子树
+    indexes_t _left_indexes, _right_indexes;
+    for (index_t i=0; i<indexes.size(); ++i) {
+        index_t __index = indexes[i];
+        //处理val与head->val相同的情况
+        if (__index == _head->index_set.front()) continue;
+        if (dataset[__index][_dim]<=_head->val) _left_indexes.push_back(__index);
+        else _right_indexes.push_back(__index);
+    }
+    _build_KDTree(_head->left, _left_indexes, max_leaf_size);
+    _build_KDTree(_head->right, _right_indexes, max_leaf_size);
+
+    //递归完成后，返回
+    return;
 }
 
 template<typename data_t>
@@ -141,11 +183,11 @@ void KDTree<data_t>::print() {
 }
 
 template<typename data_t>
-void KDTree<data_t>::_print(const TreeNode<data_t> *head) {
-    if (!head) return;
-    std::cout << head->val <<std::endl;
-    _print(head->left);
-    _print(head->right);
+void KDTree<data_t>::_print(const TreeNode<data_t> *_head) {
+    if (!_head) return;
+    _print(_head->left);
+    std::cout << _head->val << "\t";
+    _print(_head->right);
 }
 
 template<typename data_t>
@@ -179,13 +221,28 @@ typename KDTree<data_t>::points_t KDTree<data_t>::search(const point_t &p, const
 }
 
 template<typename data_t>
+indexes_t KDTree<data_t>::search_index(const point_t&  p, const unsigned int topk) {
+    if (topk<=0 || topk>data_cnt) throw std::invalid_argument("Parameter topk is invalid!\n");
+
+    std::vector<std::pair<index_t, data_t>> _indexes_dists;
+     _search(p, topk, head, _indexes_dists);
+     indexes_t res;
+     for (auto _index_dist:_indexes_dists) {
+         res.push_back(_index_dist.first);
+     }
+     return res;
+}
+
+template<typename data_t>
 void KDTree<data_t>::_search(const point_t &p, const unsigned int topk, TreeNode<data_t> *head,
-                                                                std::vector<std::pair<index_t, data_t>> &res) {
+                             std::vector<std::pair<index_t, data_t>> &res) {
     if (!head) return;
-    std::cout << "res_size" << res.size() << std::endl;
+    //std::cout << "res_size" << res.size() << std::endl;
 
     //std::vector<std::pair<indexes_t, data_t>> cur_res;
-    if (p[head->dim] < head->val) {
+    if (head->dim != std::size_t(-1)) {
+    //注意这里的分支原则，要与建树时一致
+    if (p[head->dim] <= head->val) {
         _search(p, topk, head->left, res);
         if (res.size()<topk || res.empty() || std::abs(dataset[res.back().first][head->dim]-head->val)<res.back().second) {
             _search(p, topk, head->right, res);
@@ -196,6 +253,7 @@ void KDTree<data_t>::_search(const point_t &p, const unsigned int topk, TreeNode
             _search(p, topk, head->left, res);
         }
     }
+    }// end if(head->dim != -1)
 
     //先把res灌满
     index_t i = index_t(0);
@@ -227,7 +285,30 @@ void KDTree<data_t>::_search(const point_t &p, const unsigned int topk, TreeNode
     return;
 }
 
+template<typename data_t>
+unsigned KDTree<data_t>::tree_max_depth() {
+    return _tree_max_depth(head);
 }
+
+template<typename data_t>
+unsigned KDTree<data_t>::_tree_max_depth(TreeNode<data_t>* head) {
+    return head?std::max(_tree_max_depth(head->left), _tree_max_depth(head->right))+1:0;
+}
+
+template<typename data_t>
+unsigned KDTree<data_t>::tree_min_depth() {
+    return _tree_min_depth(head);
+}
+
+template<typename data_t>
+unsigned KDTree<data_t>::_tree_min_depth(TreeNode<data_t>* head) {
+    if (!head) return 0;
+    if (head->left && !head->right) return _tree_min_depth(head->left);
+    if (!head->left && head->right) return _tree_min_depth(head->right);
+    return std::min(_tree_min_depth(head->left), _tree_min_depth(head->right))+1;
+}
+
+}// end namespace
 
 
 #endif // KDTREE_HPP
